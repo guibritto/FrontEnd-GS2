4d9c168a3ca9674608699462cf13e47ea8dd0597
guibritto <161324265+guibritto@users.noreply.github.com>
2024-11-21 23:15:41 -0300

    Adicionando Método PUT (API Restful)

--- front_gsnw/src/app/api/previsao_res/[id]/routes.ts
+++ front_gsnw/src/app/api/previsao_res/[id]/routes.ts
@@ -10,3 +10,19 @@ export async function GET(request: Request, { params }: { params: { id: number }
     const produto = produtos.find(p => p.id == params.id);
     return NextResponse.json(produto);
 }
+export async function PUT(request: Request, { params }: { params: { id: number } }) {
+
+    try {
+        const file = await fs.readFile(process.cwd() + '/src/data/base_previsao.json', 'utf-8')
+        const produtos: Previsao[] = JSON.parse(file)
+        const index = produtos.findIndex(p => p.id == params.id)
+        if (index != -1) {
+            const body = await request.json()
+            produtos.splice(index, 1, body)
+            await fs.writeFile(process.cwd() + '/src/data/base previsao.json', JSON.stringify(produtos))
+            return NextResponse.json({ msg: 'Previsão atualizado com sucesso' })
+        }
+    } catch (error) {
+        return NextResponse.json({ msg: 'Erro ao atualizar previsão' + error }, { status: 500 })
+    }
+}
