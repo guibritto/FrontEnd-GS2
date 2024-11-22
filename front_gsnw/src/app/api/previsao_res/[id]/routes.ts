08dfc60a7a2e80ca9fc9ab0e433f04b0ce281c05
guibritto <161324265+guibritto@users.noreply.github.com>
2024-11-21 23:16:52 -0300

    Adicionando Método DELETE (API Restful)

--- front_gsnw/src/app/api/previsao_res/[id]/routes.ts
+++ front_gsnw/src/app/api/previsao_res/[id]/routes.ts
@@ -26,3 +26,20 @@ export async function PUT(request: Request, { params }: { params: { id: number }
         return NextResponse.json({ msg: 'Erro ao atualizar previsão' + error }, { status: 500 })
     }
 }
+
+export async function DELETE(request: Request, { params }: { params: { id: number } }) {
+
+    try {
+        const file = await fs.readFile(process.cwd() + '/src/data/base_previsao.json', 'utf-8')
+        const produtos: Previsao[] = JSON.parse(file)
+        const index = produtos.findIndex(p => p.id == params.id)
+        if (index != -1) {
+            produtos.splice(index, 1)
+            await fs.writeFile(process.cwd() + '/src/data/base_previsao.json', JSON.stringify(produtos))
+            return NextResponse.json({ msg: "Produto apagado com sucesso" })
+        }
+
+    } catch (error) {
+        return NextResponse.json({ msg: "Erro ao apagar o produto" + error }, { status: 500 })
+    }
+}
